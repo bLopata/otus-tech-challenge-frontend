2e4759243fa5bbaa6c246defcf1cad78ebfad82f
bLopata <benlopata@gmail.com>
2020-02-01 14:34:50 -0600

    Removed search filter package.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-// search module
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-
 import { AppComponent } from './app.component';
 import { SearchComponentComponent } from './search-component/search-component.component';
 import { DetailsComponentComponent } from './details-component/details-component.component';
 import { CourseUtilsService } from './course-utils.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, Ng2SearchPipeModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, SearchComponentComponent, DetailsComponentComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CourseUtilsService]
