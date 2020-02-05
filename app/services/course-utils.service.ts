4d42f80a9ea06f2cc4c4313f08553d65de0cf846
bLopata <benlopata@gmail.com>
2020-02-05 15:38:15 -0600

    Added link styling to student-search, and implemented report card in student-details. 👨‍🎓

--- app/services/course-utils.service.ts
+++ app/services/course-utils.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import schoolData from "../schoolData.json";
 import { Observable } from "rxjs";
-import { of } from 'rxjs/observable/of';
+import { of } from "rxjs/observable/of";
 import { map } from "rxjs/operators";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 // @ts-ignore
 import { Student } from "../models/Student.ts";
 
@@ -24,18 +24,40 @@ export class CourseUtilsService {
   }
 
   listClassesById(studentClasses) {
-    return studentClasses.map(el => schoolData.classes[el.id]).join(", ");
+    let letterGrades = {
+      4: "A",
+      3.5: "A-",
+      3: "B",
+      2.5: "B-",
+      2: "C",
+      1.5: "C-",
+      1: "D",
+      0.5: "D-",
+      0: "F"
+    };
+
+    let grades = studentClasses
+      .map(for(letc => c["grade"])
+      .map(num => letterGrades[num]);
+      
+    let courseNames = studentClasses.map(el => schoolData.classes[el.id]);
+
+    return [courseNames, grades]
+      .reduce((a, b) => b.map((v, i) => v + " - " + a[i]))
+      .join("\r\n");
   }
 
   getStudents(): Observable<Student[]> {
     return of(this.students);
-  };
+  }
 
   getStudentById(id: number | string) {
     return this.getStudents().pipe(
-      map((students: Student[]) => students.find(student => student.student_id === +id))
+      map((students: Student[]) =>
+        students.find(student => student.student_id === +id)
+      )
     );
-  };
+  }
 
   addId(id) {
     return function iter(o) {
